aa44768abe542264e0e2725ba04a61d3a1097295
musyoku <musyoku@users.noreply.github.com>
2016-04-28 15:32:14 +0900

    j

--- embed_id.py
+++ embed_id.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 from chainer import cuda, Variable
 from chainer import links as L
 
 class EmbedID(L.EmbedID):
 
+	# vec: Variable
 	def cosine_similarity(self, vec):
 		vec = vec.data
 		W = self.W.data
@@ -15,15 +17,53 @@ class EmbedID(L.EmbedID):
 		# 最初の軸がIDに対応する
 		return inner_product / norm
 
-	def id(self, vec, sample=False):
+	# vec: Variable
+	def reverse(self, vec, sample=False, to_cpu=False, to_variable=True):
+		xp = cuda.get_array_module(*(vec.data,))
+		if sample:
+			result = self.reverse_sampling(vec)	# Numpy ndarray
+			if to_variable:
+				result = Variable(result)
+			if to_cpu or xp is np:
+				return result
+			result.to_gpu()
+			return result
+		else:
+			# 最初の軸がIDに対応する
+			result = self.reverse_argmax(vec)	# Numpy ndarray or Cupy ndarray
+			if to_variable:
+				result = Variable(result)
+				if to_cpu:
+					result.to_cpu()
+				return result
+			if to_cpu and xp is cuda.cupy:
+				result = cuda.to_cpu(result)
+			return result
+			
+	# vec: Variable
+	# Returns xp
+	def reverse_argmax(self, vec):
 		xp = cuda.get_array_module(*(vec.data,))
 		cos = self.cosine_similarity(vec)
-		if sample:
-			sum = xp.exp(xp.sum(cos, axis=0))
-			sum = sum.reshape(1, -1)
-			print xp.exp(cos)
-			print sum
-			print xp.exp(cos) / sum
-		else:
-			return Variable(xp.argmax(cos, axis=0))
+		# 最初の軸がIDに対応する
+		return xp.argmax(cos, axis=0)
 
+	# vec: Variable
+	# Returns np
+	def reverse_sampling(self, vec):
+		xp = cuda.get_array_module(*(vec.data,))
+		cos = self.cosine_similarity(vec)
+		cos = xp.exp(cos)
+		sum = xp.sum(cos, axis=0)
+		sum = sum.reshape(1, -1)
+		softmax = cos / sum
+		if xp is cuda.cupy:
+			softmax = cuda.to_cpu(softmax)
+		softmax = softmax.T
+		n_vec = softmax.shape[0]
+		n_ids = softmax.shape[1]
+		result = np.empty((n_vec,), dtype=np.int32)
+		for t in xrange(n_vec):
+			id = np.random.choice(np.arange(n_ids), p=softmax[t])
+			result[t] = id
+		return result
--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from chainer import cuda, Variable
 from embed_id import EmbedID
 
 xp = cuda.cupy
-n_ids = 3
-ndim_vec = 2
+n_ids = 100
+ndim_vec = 200
 embed = EmbedID(n_ids, ndim_vec)
 embed.to_gpu()
 
 x = Variable(xp.arange(n_ids, dtype=xp.int32))
 vec = embed(x)
-_x = embed.id(Variable(xp.asarray([vec.data[0], vec.data[1], vec.data[2], vec.data[0] + vec.data[1], vec.data[0] - vec.data[1], vec.data[2], vec.data[1]], dtype=xp.float32)), sample=True)
+_x = embed.reverse(Variable(xp.asarray([vec.data[0], vec.data[1], vec.data[2], (vec.data[0] + vec.data[1]) / 2], dtype=xp.float32)))
 print _x.data
