c76d86124b2a31de505a7d74a63722d81ad3b356
musyoku <musyoku@users.noreply.github.com>
2016-04-28 14:53:02 +0900

    j

--- embed_id.py
+++ embed_id.py
@@ -4,12 +4,26 @@ from chainer import links as L
 
 class EmbedID(L.EmbedID):
 
-	def reverse(self, vec):
+	def cosine_similarity(self, vec):
 		vec = vec.data
 		W = self.W.data
 		xp = cuda.get_array_module(*(vec,))
 		w_norm = xp.sqrt(xp.sum(W ** 2, axis=1))
 		v_norm = xp.sqrt(xp.sum(vec ** 2, axis=1))
-		product = W.dot(vec.T)
+		inner_product = W.dot(vec.T)
 		norm = w_norm.reshape(1, -1).T.dot(v_norm.reshape(1, -1)) + 1e-6
-		return Variable(xp.argmax(product / norm, axis=0))
+		# 最初の軸がIDに対応する
+		return inner_product / norm
+
+	def id(self, vec, sample=False):
+		xp = cuda.get_array_module(*(vec.data,))
+		cos = self.cosine_similarity(vec)
+		if sample:
+			sum = xp.exp(xp.sum(cos, axis=0))
+			sum = sum.reshape(1, -1)
+			print xp.exp(cos)
+			print sum
+			print xp.exp(cos) / sum
+		else:
+			return Variable(xp.argmax(cos, axis=0))
+
--- test.py
+++ test.py
@@ -10,5 +10,5 @@ embed.to_gpu()
 
 x = Variable(xp.arange(n_ids, dtype=xp.int32))
 vec = embed(x)
-_x = embed.reverse(Variable(xp.asarray([vec.data[0], vec.data[1], vec.data[2], vec.data[0] + vec.data[1], vec.data[0] - vec.data[1], vec.data[2], vec.data[1]], dtype=xp.float32)))
+_x = embed.id(Variable(xp.asarray([vec.data[0], vec.data[1], vec.data[2], vec.data[0] + vec.data[1], vec.data[0] - vec.data[1], vec.data[2], vec.data[1]], dtype=xp.float32)), sample=True)
 print _x.data
